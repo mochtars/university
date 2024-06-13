13d14a6eb25f14e7f4b9187a310026bddb93ea7c
mochtars <benimochtars@gmail.com>
2024-06-13 18:25:42 +0700

    init

--- semester_2/sistem_basis_data/tugas1.sql
+++ semester_2/sistem_basis_data/tugas1.sql
@@ -1,1 +1,38 @@
 /* 8 June 2024 */
+
+-- Membuat database baru dengan nama SBD
+CREATE DATABASE SBD;
+-- USE chats;
+
+-- Membuat tabel chats
+CREATE TABLE chats (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  sender_id INT NOT NULL,
+  receiver_id INT NOT NULL,
+  message_text VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Memasukkan data ke dalam tabel chats
+INSERT INTO chats (sender_id, receiver_id, message_text)
+VALUES
+  (28, 5, 'Halo, apa kabs?'),
+  (5, 28, 'Halo, saya baik, anda bagaimana'),
+  (28, 5, 'kurang baik :('),
+  (5, 28, 'loh anda kurang baik kenapa?'),
+  (28, 5, 'tugas sistem basis data saya hilang :('),
+  (5, 28, 'kok bisa hilang?'),
+  (28, 5, 'ditelan komodo peliharaan saya :('),
+  (5, 28, 'dhlh.'),
+  (5, 11, 'Kak, lgi sibuk ga?'),
+  (11, 5, 'ngga, knp dek?'),
+  (5, 11, 'gpp.');
+
+-- Menampilkan semua data
+SELECT * FROM chats;
+-- Menampilkan id dan message_text dengan sender_id = 5 dari tabel chats 
+SELECT id, message_text FROM chats WHERE sender_id = 5;
+
+-- Mengubah message_text dari id = 7
+UPDATE chats SET message_text = "ditelan kucing peliharaan saya :()"
+WHERE id = 7;
